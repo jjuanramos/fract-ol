c9406679598d353ed7782c432a2bc4596c4d945b
juramos <juramos@student.42madrid.com>
2024-01-10 13:22:12 +0100

    cleaned it up a little

---
+++ fractol.h
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juramos <juramos@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/10 13:10:41 by juramos           #+#    #+#             */
+/*   Updated: 2024/01/10 13:14:53 by juramos          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRACTOL_H
+# define FRACTOL_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <mlx.h>
+# include <math.h>
+
+typedef struct s_img {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}			t_img;
+
+typedef struct s_render {
+	void	*mlx;
+	void	*win;
+}			t_render;
+
+typedef struct s_complex {
+	double	real;
+	double	imag;
+}			t_complex;
+
+double		abs_complex(t_complex c);
+t_complex	add_complex(t_complex a, t_complex b);
+t_complex	square_complex(t_complex c);
+
+#endif
--- main.c
+++ main.c
@@ -6,83 +6,29 @@
 /*   By: juramos <juramos@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 10:10:40 by juramos           #+#    #+#             */
-/*   Updated: 2024/01/10 12:55:18 by juramos          ###   ########.fr       */
+/*   Updated: 2024/01/10 13:20:49 by juramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <mlx.h>
-#include <math.h>
+#include "fractol.h"
 
-typedef struct s_data {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}			t_data;
-
-typedef struct s_vars {
-	void	*mlx;
-	void	*win;
-}			t_vars;
-
-typedef struct Complex {
-	double	real;
-	double	imag;
-}			t_complex;
-
-enum {
-	ON_KEYDOWN = 2,
-	ON_KEYUP = 3,
-	ON_MOUSEDOWN = 4,
-	ON_MOUSEUP = 5,
-	ON_MOUSEMOVE = 6,
-	ON_EXPOSE = 12,
-	ON_DESTROY = 17
-};
-
-double	abs_complex(t_complex c)
+int	close(int keycode, t_render *render)
 {
-	return (sqrt(pow(c.real, 2) + pow(c.imag, 2)));
-}
-
-t_complex	add_complex(t_complex a, t_complex b)
-{
-	t_complex	c;
-
-	c.real = a.real + b.real;
-	c.imag = a.imag + b.imag;
-	return (c);
-}
-
-t_complex	square_complex(t_complex c)
-{
-	t_complex	n;
-
-	n.real = pow(c.real, 2) - pow(c.imag, 2);
-	n.imag = 2 * c.real * c.imag;
-	return (n);
-}
-
-int	close(int keycode, t_vars *vars)
-{
-	mlx_destroy_window(vars->mlx, vars->win);
+	mlx_destroy_window(render->mlx, render->win);
 	return (0);
 }
 
-int	close_exit(t_vars *vars)
+int	close_exit(t_render *render)
 {
-	mlx_destroy_window(vars->mlx, vars->win);
+	mlx_destroy_window(render->mlx, render->win);
 	exit(0);
 }
 
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+void	my_mlx_pixel_put(t_img *img, int x, int y, int color)
 {
 	char	*dst;
 
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	dst = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
 	*(unsigned int *)dst = color;
 }
 
@@ -101,36 +47,12 @@ int	mandelbrot(t_complex c)
 	return (n);
 }
 
-unsigned int interpolate_color(unsigned int color1, unsigned int color2, int n, int maxN) {
-    // Extract the red, green, and blue components of color1
-    int r1 = (color1 >> 16) & 0xFF;
-    int g1 = (color1 >> 8) & 0xFF;
-    int b1 = color1 & 0xFF;
-
-    // Extract the red, green, and blue components of color2
-    int r2 = (color2 >> 16) & 0xFF;
-    int g2 = (color2 >> 8) & 0xFF;
-    int b2 = color2 & 0xFF;
-
-    // Calculate interpolated red component
-    int r = r1 + (r2 - r1) * n / maxN;
-
-    // Calculate interpolated green component
-    int g = g1 + (g2 - g1) * n / maxN;
-
-    // Calculate interpolated blue component
-    int b = b1 + (b2 - b1) * n / maxN;
-
-    // Combine the interpolated components to form the resulting color
-    return (r << 16) | (g << 8) | b;
-}
-
 // real = x * (x_max - x_min) / (width - 1) + x_min
 // imag = y * (y_max - y_min) / (height - 1) + y_min
 // we apply the formula above with x_max, y_max = 2 and x_min, y_min = -2,
 // this formula will give to our real and imag values the proportionally equal
 // values in that scale.
-void	paint_whole_window(t_data *data, int width, int length)
+void	paint_whole_window(t_img *img, int width, int length)
 {
 	int				x;
 	int				y;
@@ -147,27 +69,26 @@ void	paint_whole_window(t_data *data, int width, int length)
 			c.real = x * 4 / (width - 1) - 2;
 			c.imag = y * 4 / (length - 1) - 2;
 			n = mandelbrot(c);
-			color = interpolate_color(0xFFFFFFFF, 0xFF000000, n, 100);
-			my_mlx_pixel_put(data, x, y, color);
+			my_mlx_pixel_put(img, x, y, 0xFFFFFFFF + n);
 		}
 	}
 }
 
 int	main(void)
 {
-	void	*mlx;
-	void	*mlx_win;
-	t_data	img;
-	t_vars	vars;
+	void		*mlx;
+	void		*mlx_win;
+	t_img		img;
+	t_render	render;
 
-	vars.mlx = mlx_init();
-	vars.win = mlx_new_window(vars.mlx, 800, 800, "Hello world!");
-	img.img = mlx_new_image(vars.mlx, 800, 800);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-		&img.endian);
+	render.mlx = mlx_init();
+	render.win = mlx_new_window(render.mlx, 800, 800, "Hello world!");
+	img.img = mlx_new_image(render.mlx, 800, 800);
+	img.addr = mlx_get_data_addr(
+			img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
 	paint_whole_window(&img, 800, 800);
-	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
-	mlx_hook(vars.win, 2, 1L << 0, close, &vars);
-	mlx_hook(vars.win, 17, 1L << 1, close_exit, &vars);
-	mlx_loop(vars.mlx);
+	mlx_put_image_to_window(render.mlx, render.win, img.img, 0, 0);
+	mlx_hook(render.win, 2, 1L << 0, close, &render);
+	mlx_hook(render.win, 17, 1L << 1, close_exit, &render);
+	mlx_loop(render.mlx);
 }
